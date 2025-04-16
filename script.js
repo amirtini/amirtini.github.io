72b03fa84725f471d964fa207aaedf360109d182
Amirtini <amir967@mail.ru>
2025-04-16 08:24:27 +0300

    added new picture

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const obj = {name:"lolkek"}
-console.log(obj.name)
+// Получаем все кнопки навигации
+const navButtons = document.querySelectorAll('.nav-button');
+
+// Добавляем обработчик события для каждой кнопки
+navButtons.forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        const tooltip = button.querySelector('.tooltip');
+        tooltip.style.display = 'block'; // Показываем подсказку
+        tooltip.style.opacity = '1'; // Устанавливаем непрозрачность
+    });
+
+    button.addEventListener('mouseleave', () => {
+        const tooltip = button.querySelector('.tooltip');
+        tooltip.style.display = 'none'; // Скрываем подсказку
+        tooltip.style.opacity = '0'; // Устанавливаем непрозрачность
+    });
+});
+
